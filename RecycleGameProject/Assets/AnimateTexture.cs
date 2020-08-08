dc7250b02b01a99e8ee868a5a48157aa00f0007d
Bailey <baileygardner25@gmail.com>
2020-08-08 20:33:19 +0100

    Assets and scripts

---
+++ RecycleGameProject/Assets/AnimateTexture.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateTexture : MonoBehaviour {
+
+    public float moveX = 0.5f;
+    public float moveY = 0.5f;
+
+    // Update is called once per frame
+    void Update() {
+        float offsetX = Time.time * moveX;
+        float offsetY = Time.time * moveY;
+
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(offsetX, offsetY);
+    }
+}
---
+++ RecycleGameProject/Assets/MoveItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveItem : MonoBehaviour
+{
+    private Vector3 startPos;
+    private Vector3 movePos;
+    public float MovementSpeed = 0.5f;
+    public float BobMagnitude = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movePos = new Vector3(MovementSpeed * Time.time, Mathf.Sin(Time.time * 4.5f) * BobMagnitude, 0.0f);
+        transform.position = startPos + movePos;
+    }
+}
