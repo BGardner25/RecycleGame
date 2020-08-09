3a0ad83e46ade5c7b85c4fee669a10baca8f1920
Bailey <baileygardner25@gmail.com>
2020-08-09 16:40:41 +0100

    UI
    Added score UI element, pause UI element and pause/resume functionality

---
+++ RecycleGameProject/Assets/Scripts/PauseScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour
+{
+
+    // toggle between pause and resume
+    public void TogglePause()
+    {
+        bool isPaused = System.Convert.ToBoolean(Time.timeScale);
+        isPaused = !isPaused;
+        Time.timeScale = System.Convert.ToInt32(isPaused);
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+    }
+}
---
+++ RecycleGameProject/Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour
+{
+    private int score = 0;
+    public Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // set score to 0?   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "Score: " + score;
+
+        // on correct collision, increase score
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            score+=100;
+        }
+    }
+}
