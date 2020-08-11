db064de4217acd66d0ef9687ed5a8c6b67d6b213
Bailey <baileygardner25@gmail.com>
2020-08-11 14:00:12 +0100

    fixes

---
+++ RecycleGameProject/Assets/Scripts/ClickAndDrag.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickAndDrag : MonoBehaviour
+{
+    private Vector2 startPos;
+
+    void OnMouseDown()
+    {
+        startPos = transform.position;
+        //this.GetComponent<Rigidbody2D>().gravityScale = 0.0f;
+        // store original pos
+    }
+
+    void OnMouseDrag()
+    {
+        Debug.Log("dragging");
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        transform.Translate(mousePos);
+    }
+
+    void OnMouseUp()
+    {
+        transform.position = startPos;
+    }
+}
