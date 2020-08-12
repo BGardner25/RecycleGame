ad6d45f3d3d4fc397d1e28d004ddf1ecdc6f60dc
Bailey <baileygardner25@gmail.com>
2020-08-12 18:10:45 +0100

    screen shake

--- RecycleGameProject/Assets/Scripts/HealthBarScript.cs
+++ RecycleGameProject/Assets/Scripts/HealthBarScript.cs
@@ -2,22 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEditor;
 
 public class HealthBarScript : MonoBehaviour
 {
 
     public List<GameObject> healthBar;
-    private int count = 5;
     public Sprite greyHeart;
+    public Sprite redHeart;
+    public ScreenShakeScript screenShake;
+    private int count = 5;
+    private List<GameObject> fallingHearts;
+    private bool[] isHeartsFalling;
+    private GameObject clone;
 
     // Start is called before the first frame update
     void Start()
     {
         healthBar = new List<GameObject>();
+        fallingHearts = new List<GameObject>();
         foreach (Transform child in transform)
+        {
+            clone = Instantiate(child.gameObject);
+            clone.GetComponent<Image>().sprite = redHeart;
+            GameObjectUtility.SetParentAndAlign(clone, child.gameObject);
+            fallingHearts.Add(clone);
             healthBar.Add(child.gameObject);
+        }
         healthBar.Reverse();
+        fallingHearts.Reverse();
         count = healthBar.Count;
+        isHeartsFalling = new bool[count];
+    }
+
+    void Update()
+    {
+        for(int i = isHeartsFalling.Length - 1; i > 0; i--)
+            if (isHeartsFalling[i])
+                if (fallingHearts[i].transform.position.y >= -40.0f)
+                    fallingHearts[i].transform.Translate(0.0f, -1.0f, 0.0f);
     }
 
     public int GetHealthCount()
@@ -32,6 +55,9 @@ public class HealthBarScript : MonoBehaviour
 
     public void RemoveHealth()
     {
+        screenShake.ScreenShake();
+        //Instantiate(fallingHeart, healthBar[count - 1].transform.position, Quaternion.identity);
+        isHeartsFalling[count - 1] = true;
         healthBar[count - 1].GetComponent<Image>().sprite = greyHeart;
         count--;
     }
---
+++ RecycleGameProject/Assets/Scripts/ScreenShakeScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+* Original implementation by Matt Buckley
+* https://medium.com/@mattThousand/basic-2d-screen-shake-in-unity-9c27b56b516
+*/
+
+public class ScreenShakeScript : MonoBehaviour
+{
+    Vector3 startPos;
+    private Transform tF;
+    private float shakeTime = 0.0f;
+    private float magnitude = 0.015f;
+    private float damping = 3.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(shakeTime > 0)
+        {
+            transform.localPosition = startPos + Random.insideUnitSphere * magnitude;
+            shakeTime -= Time.deltaTime * damping;
+        }
+        else
+        {
+            shakeTime = 0.0f;
+            transform.localPosition = startPos;
+        }
+    }
+
+    public void ScreenShake()
+    {
+        shakeTime = 1.0f;
+    }
+
+    void Awake()
+    {
+        if (!transform)
+            tF = GetComponent(typeof(Transform)) as Transform;
+    }
+
+    void OnEnable()
+    {
+        startPos = transform.localPosition;
+    }
+}
