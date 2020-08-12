693be6e6d2bd27f2a6155dba709794657b7e2155
Bailey <baileygardner25@gmail.com>
2020-08-12 21:06:24 +0100

    out of bounds
    added out of bounds at end of river and tweaked buoyancy

---
+++ RecycleGameProject/Assets/Scripts/OutOfBounds.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutOfBounds : MonoBehaviour
+{
+    public HealthBarScript healthBar;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.transform.parent.tag == "RecycleItem")
+        {
+            Destroy(other.gameObject);
+            healthBar.RemoveHealth();
+        }
+
+    }
+            
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
