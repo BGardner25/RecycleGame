61a7bb508a22e7e8611680faaaca234b555d5426
Bailey <baileygardner25@gmail.com>
2020-08-13 16:50:47 +0100

    fixed default values for audio

--- RecycleGameProject/Assets/Scripts/Sound.cs
+++ RecycleGameProject/Assets/Scripts/Sound.cs
@@ -11,8 +11,8 @@ public class Sound
     public AudioSource aSrc;
 
     [Range(0.0f, 1.0f)]
-    public float volume;
-    [Range(0.1f, 1.0f)]
-    public float pitch;
+    public float volume = 1.0f;
+    [Range(0.1f, 2.0f)]
+    public float pitch = 1.0f;
 
 }
