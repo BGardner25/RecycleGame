66ce51bf21876430a9b036ccd480c49f6263dfe4
Bailey <baileygardner25@gmail.com>
2020-08-14 21:02:32 +0100

    tutorial for level1

--- RecycleGameProject/Assets/Scripts/TutorialManager.cs
+++ RecycleGameProject/Assets/Scripts/TutorialManager.cs
@@ -7,6 +7,7 @@ public class TutorialManager : MonoBehaviour
     public static bool isTutorialActive = false;
     public MainUI mainUI;
     public GameObject darkScreen;
+    public GameObject TutorialPanel;
     public List<GameObject> tutMessages;
 
     void Awake()
@@ -25,12 +26,23 @@ public class TutorialManager : MonoBehaviour
         }
     }
 
+    public void DisplayTutorial(GameObject tutObject)
+    {
+        isTutorialActive = true;
+        darkScreen.SetActive(true);
+        FindObjectOfType<ItemManager>().FreezeAllItems();
+        mainUI.SetMainUIVisibility(false);
+        TutorialPanel.SetActive(true);
+        tutObject.SetActive(true);
+    }
+
     public void RemoveTutorialMessage(string tag)
     {
         foreach (GameObject tutObject in tutMessages)
             if (string.Equals(tutObject.tag, tag, System.StringComparison.OrdinalIgnoreCase))
             {
                 tutObject.SetActive(false);
+                TutorialPanel.SetActive(false);
                 tutMessages.Remove(tutObject);
                 darkScreen.SetActive(false);
                 isTutorialActive = false;
@@ -38,14 +50,5 @@ public class TutorialManager : MonoBehaviour
                 mainUI.SetMainUIVisibility(true);
                 return;
             }
-    }
-
-    public void DisplayTutorial(GameObject tutObject)
-    {
-        isTutorialActive = true;
-        darkScreen.SetActive(true);
-        FindObjectOfType<ItemManager>().FreezeAllItems();
-        mainUI.SetMainUIVisibility(false);
-        tutObject.SetActive(true);
     }
 }
